11a5ce3955c84dcfad9232f07d47f5fa8cb4901a
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2022-02-08 22:09:14 -0300

    feat: add database sync when start app

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Sequelize } from 'sequelize-typescript';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private sequelize: Sequelize) {
+    this.syncDataBase();
+  }
+
+  async syncDataBase() {
+    await this.sequelize.sync();
   }
 }
