20d926ee24cc1d95f3be0874385cada58ae21fa6
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2022-02-11 11:52:53 -0300

    refactor: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,19 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config/dist/config.module';
-import { SequelizeModule } from '@nestjs/sequelize/dist/sequelize.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { Book } from './book.model';
-import { BooksController } from './books.controller';
-import { BooksService } from './books.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getConnectionOptions } from 'typeorm';
+import { BookModule } from './books/book.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    SequelizeModule.forRoot({
-      dialect: 'mysql',
-      host: process.env.MYSQL_HOST,
-      port: Number(process.env.MYSQL_PORT),
-      username: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DB_NAME,
-      autoLoadModels: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      useFactory: async () =>
+        Object.assign(await getConnectionOptions(), {
+          autoLoadEntities: true,
+        }),
     }),
-    SequelizeModule.forFeature([Book]),
+    BookModule,
   ],
-  controllers: [AppController, BooksController],
-  providers: [AppService, BooksService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
